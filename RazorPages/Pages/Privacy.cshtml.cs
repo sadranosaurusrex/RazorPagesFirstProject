760b0c34a22498b2df9a82cc6dd671db58cca730
sadranosaurusrex <msmohamadi1385@gmail.com>
2025-12-21 12:47:12 +0330

    displaying date on privacy page

--- RazorPages/Pages/Privacy.cshtml.cs
+++ RazorPages/Pages/Privacy.cshtml.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPages.Pages
 {
     public class PrivacyModel : PageModel
     {
+        private const string V = "TimeStamp";
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData[V] = dateTime;
         }
     }
 
---
+++ RazorPages/Pages/sadra.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages
+{
+    public class sadraModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
